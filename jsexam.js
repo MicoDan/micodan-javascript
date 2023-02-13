89d90aac09eca0d30cecdc87ad0d9ee273bf7dd8
Mico Dan <micodan369@gmail.com>
2023-02-13 16:02:00 +0200

    update commit

--- jsexam.js
+++ jsexam.js
@@ -3,11 +3,11 @@ const app = express();
 //this represents the GET API to do addition of list of numbers and return their sum
 
 /* i, */app.get('/add', (req, res) => {
-    const numbers = req.query.numbers;
+    const numbers = req.body.numbers;
     let sum = 0
-    const arr = numbers.split(',')
-    for(i=0; i<arr.length; i++){
-    sum = sum + arr[i]
+    const arr = numbers.split(',').map(Number)
+    for(const numb of arr){
+    sum = sum + numb
     }
     res.send(`The sum of numbers is: ${sum}`);
 });
@@ -15,11 +15,11 @@ const app = express();
 //the below code represents the GET API to do multiplication of an array of numbers and return the product
 
 /* ii, */ app.get('/multiply', (req, res) => {
-    const numbers = req.query.numbers;
+    const numbers = req.body.numbers;
     let product = 1
-    const nums = numbers.split(',')
-    for(j=0; j<nums.length; j++){
-        product = product * nums[j]
+    const nums = numbers.split(',').map(Number)
+    for(const num of nums){
+        product = product * num
     }
     res.send(`The product of numbers is: ${product}`);
 });
